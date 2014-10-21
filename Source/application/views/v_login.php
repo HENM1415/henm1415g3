e181bc08ad97fd46bbf4feeee5d5c7ac75ee7376
Marko Jovanovic <marko@jovanovic.rs>
2014-10-21 10:16:50 +0200

    Forgot to commit a view file...

---
+++ Source/application/views/v_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Welcome to AlmostPerfect, the social network of Aachen.</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+</head>
+<body>
+
+<div class="background"></div>
+<div class="header">
+    <div>Almost<span>Perfect</span></div>
+</div>
+
+<form class="user-form" action="<?php echo site_url('login'); ?>" method="post">
+
+	<?php if (isset($wrong_password)): ?>
+	<div style="color: #CC0000; ">Wrong username and/or password. Please try again.</div>
+	<?php endif; ?>
+    <input type="email" name="email" placeholder="email" />
+    <input type="password" name="password" placeholder="password" />
+    <input type="submit" name="submit" value="Login" />
+    <input type="button" value="Register" />
+</form>
+
+</body>
+</html>
